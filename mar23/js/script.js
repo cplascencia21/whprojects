729234e132169e2b32ae3c458ffca1a9a34f91b2
unknown <cplascencia21@yahoo.com>
2018-03-23 13:51:55 -0700

    challenge

---
+++ mar23/js/script.js
@@ -0,0 +1,45 @@
+// all variables
+
+var up = document.getElementById("up");
+up.style.color = "red"; 
+up.style.fontSize = "20px";
+up.style.backgroundColor = "yellow";
+
+var  down = document.getElementById("down");
+down.style.color = "blue";
+down.style.fontSize = "20px";
+down.style.backgroundColor = "lightblue";
+
+
+var right = document.getElementById("right");
+right.style.color = "green";
+right.style.fontSize = "20px";
+right.style.backgroundColor = "lightgreen";
+
+var link = document.getElementById("link");
+
+
+var welcome = document.getElementById("welcome");
+
+var lorem = document.getElementById("lorem");
+
+var seek = document.getElementById("seek");
+
+var out = document.getElementById("out");
+
+// all eventlistener
+
+up.addEventListener("click");
+
+down.addEventListener("click");
+
+right.addEventListener("click");
+
+seek.addEventListener("click");
+
+out.addEventListener("click");
+
+// all functions
+
+
+
